4122627a77e4bce0e0914f4d13ca7c8bdb192705
lorenuars19 <lorenuar.s19@gmail.com>
2024-08-04 01:03:22 +0200

    fix autorun.cmd

--- autorun.cmd
+++ autorun.cmd
@@ -1,10 +1,10 @@
 
-SET PIXILANG_FOLDER="pixilang-3.8.4\pixilang\pixilang3\bin\windows_x86_64"
+SET PIXILANG_FOLDER="..\pixilang-3.8.4\pixilang\pixilang3\bin\windows_x86_64"
 SET PIXILANG_EXE="%PIXILANG_FOLDER%\pixilang_console.exe"
-SET SUNVOX_FOLDER="sunvox-2.1.1c\sunvox\sunvox\windows_x86_64"
+SET SUNVOX_FOLDER="..\sunvox-2.1.1c\sunvox\sunvox\windows_x86_64"
 SET SUNVOX_EXE="sunvox.exe"
-SET SUNVOX_TEST_FILE="%PIXILANG_FOLDER%\test.sunvox"
-SET PIXI_FILE="spectral-compressor-generator.pixi"
+SET SUNVOX_TEST_FILE="test.sunvox"
+SET PIXI_FILE="meta-patcher.pixi"
 
 
 taskkill /im %SUNVOX_EXE% /f /t
