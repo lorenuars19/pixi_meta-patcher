6e833e9e6a944dbceede89cca2920d384e84ace1
root <root@FreakyWaves>
2024-08-04 00:56:19 +0200

    meta-patcher

---
+++ autorun.cmd
@@ -0,0 +1,13 @@
+
+SET PIXILANG_FOLDER="pixilang-3.8.4\pixilang\pixilang3\bin\windows_x86_64"
+SET PIXILANG_EXE="%PIXILANG_FOLDER%\pixilang_console.exe"
+SET SUNVOX_FOLDER="sunvox-2.1.1c\sunvox\sunvox\windows_x86_64"
+SET SUNVOX_EXE="sunvox.exe"
+SET SUNVOX_TEST_FILE="%PIXILANG_FOLDER%\test.sunvox"
+SET PIXI_FILE="spectral-compressor-generator.pixi"
+
+
+taskkill /im %SUNVOX_EXE% /f /t
+del %SUNVOX_TEST_FILE%
+%PIXILANG_EXE% %PIXI_FILE%
+if exist %SUNVOX_TEST_FILE% %SUNVOX_EXE% %SUNVOX_TEST_FILE%
